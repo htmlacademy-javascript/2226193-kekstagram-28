eed23b624b4257236ec86d73c7c60135dc796370
Anastasia <semenova.nastenka@gmail.com>
2023-02-20 22:05:22 +0300

    Уточняет решение после ретро

--- js/functions.js
+++ js/functions.js
@@ -1,28 +1,25 @@
 /* Функция для проверки длины строки. Она нам пригодится для валидации формы*/
 
-function checkLength (fieldName, minLenght) {
-  const length = fieldName.length;
-
-  return length > minLenght;
-}
+const checkLength = (fieldName, maxLenght) => fieldName.length <= maxLenght;
 
 checkLength('проверяемая строка', 20);
 
 /* Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
 предусмотрите случай, когда в строке встречаются пробелы. Они не должны учитываться при проверке!*/
 
-function checkPalindrome (string) {
-  string = string.replaceAll(' ', '');
+const isPalindrome = (string) => {
+  const tempString = string
+    .toLowerCase()
+    .replaceAll(' ', '');
+
   let i = 0;
-  let j = string.length - 1;
+  let j = tempString.length - 1;
   let letterOne;
   let letterTwo;
 
   while (i < j && i !== j) {
-    letterOne = string.at(i);
-    letterTwo = string.at(j);
-    letterOne = letterOne.toLowerCase();
-    letterTwo = letterTwo.toLowerCase();
+    letterOne = tempString.at(i);
+    letterTwo = tempString.at(j);
 
     if (letterOne !== letterTwo) {
       return false;
@@ -33,37 +30,35 @@ function checkPalindrome (string) {
   }
 
   return true;
-}
+};
 
-checkPalindrome('Лёша на полке клопа нашёл ');
+isPalindrome('Лёша на полке клопа нашёл ');
 
 /* Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
 */
 
-function getNumbers (string) {
-  string = string.replaceAll(' ', '');
-  let letter;
+const getNumbers = (string) => {
+  if (typeof string === 'number') {
+    return string;
+  }
+
   let result = '';
-
   for (let i = 0; i <= string.length - 1; i++) {
-    letter = string.at(i);
-
-    if (isNaN(Number(letter))) {
+    if (isNaN(parseInt(string.at(i), 10))) {
       continue;
     }
 
-    result = result + letter;
+    result += string.at(i);
   }
-
-  return Number(result);
-}
+  return parseInt(result, 10);
+};
 
 getNumbers('1 кефир, 0.5 батона');
 
 /* Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
 Эта функция нам пригодится для формирования адресов файлов.*/
 
-function createPath (string, minLenght, extraSymbols) {
+const createPath = (string, minLenght, extraSymbols) => {
 
   if (string.length < minLenght) {
     let emptyLength = minLenght - string.length;
@@ -87,6 +82,6 @@ function createPath (string, minLenght, extraSymbols) {
   }
 
   return string;
-}
+};
 
 createPath('qwerty', 4, '0');
